53189d28e3d1c45ebffeb2b5bc9f6ad806241972
Omar <omarihabelsayed@gmail.com>
2022-11-17 02:05:10 +0200

    Fixing UI

--- Assets/Scripts/Misc/AlertManager.cs
+++ Assets/Scripts/Misc/AlertManager.cs
@@ -26,20 +26,22 @@ public class AlertManager : MonoBehaviour
     }
     void ShowAlertPanel(Planets planet)
     {
+        Debug.Log("ALERT CALLED");
+        Debug.Log(seqCount);
 
         alertSeq1 = DOTween.Sequence();
         alertSeq1.Pause();
         alertSeq1.Append(alertPanel.DOAnchorPosY(alertPanel.anchoredPosition.y - 275, 1));
         alertSeq1.Append(planetImage.transform.DOScale(1, 0.2f));
         alertSeq1.Append(planetImage.transform.DORotate(new Vector3(0, 0, 180), 1).SetLoops(2, LoopType.Incremental)).SetEase(Ease.Linear);
-        alertSeq1.Append(alertPanel.DOAnchorPosY(alertPanel.anchoredPosition.y + 275, 1));
+        alertSeq1.Append(alertPanel.DOAnchorPosY(390, 1));
         alertSeq1.Join(planetImage.transform.DOScale(0, 0.4f));
         alertSeq2 = DOTween.Sequence();
         alertSeq2.Pause();
         alertSeq2.Append(alertPanel.DOAnchorPosY(alertPanel.anchoredPosition.y - 275, 1));
         alertSeq2.Append(planetImage.transform.DOScale(1, 0.2f));
         alertSeq2.Append(planetImage.transform.DORotate(new Vector3(0, 0, 180), 1).SetLoops(2, LoopType.Incremental)).SetEase(Ease.Linear);
-        alertSeq2.Append(alertPanel.DOAnchorPosY(alertPanel.anchoredPosition.y + 275, 1));
+        alertSeq2.Append(alertPanel.DOAnchorPosY(390, 1));
         alertSeq2.Join(planetImage.transform.DOScale(0, 0.4f));
         if (seqCount == 0)
         {
@@ -50,7 +52,9 @@ public class AlertManager : MonoBehaviour
             void Complete()
             {
                 seqCount--;
+                alertSeq1.Complete();
             }
+            Debug.Log("ALERT CALLED 1");
         }
         else
         {
@@ -63,7 +67,10 @@ public class AlertManager : MonoBehaviour
                 planetImage.sprite = planets[(int)planet];
                 alertSeq2.Play();
                 seqCount--;
+                alertSeq1.Complete();
             }
+            Debug.Log("ALERT CALLED 2");
+
         }
 
 
